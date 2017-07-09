eecb7669bda6e11dd5e3d10f6b0f7162112f593c
Livio <liviozanardo@gmail.com>
2017-07-09 18:15:43 -0400

    Generated Update and Destroy functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article =Article.new(article_params)
+    @article = Article.new(article_params)
 
     if @article.save
     redirect_to(@article)
@@ -15,6 +15,24 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @articles =Article.all
   end
 
 private
